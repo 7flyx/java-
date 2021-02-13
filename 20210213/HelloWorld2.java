a91d5fabfc996e87cebb0cd9e21182a118327d21
7flyx <2647282717@qq.com>
2021-02-13 19:56:11 +0800

    int 类型的基本操作

---
+++ 20210213/HelloWorld2.java
@@ -0,0 +1,14 @@
+
+
+public class HelloWorld2 {
+	public static void main(String args[]) {
+		
+		int a = 10;
+							//Integer--包装类
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);  //打印且换行
+		//System.out.printf("%d\n",a);  //格式化打印
+		//System.out.print(a );  //打印 但不换行
+	}
+	
+}
