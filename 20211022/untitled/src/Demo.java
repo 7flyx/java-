3b313e949e9feed64d0340168e21312abf389edf
7flyx <2647282717@qq.com>
2021-10-22 21:51:22 +0800

    SBTree

---
+++ 20211022/untitled/src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+/**
+ * Created by Terry
+ * User: Administrator
+ * Date: 2021-10-22
+ * Time: 16:28
+ * Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+    }
+}
