35db325caec072b4738f7289c58d229dfa4700f9
7flyx <2647282717@qq.com>
2022-03-01 21:35:28 +0800

    剑指offer

--- 20220206/untitled/src/LeetCode49_UglyNumber.java
+++ 20220206/untitled/src/LeetCode49_UglyNumber.java
@@ -32,6 +32,37 @@ public class LeetCode49_UglyNumber {
         return res;
     }
 
+    class Solution {
+        public int nthUglyNumber(int n) {
+            if(n < 7) {
+                return n;
+            }
+
+            int[] dp = new int[n];
+            dp[0] = 1; //第一个丑叔
+            int a = 0, b = 0,c = 0;
+            int va = 0, vb = 0, vc = 0;
+            for (int i = 1; i < n; i++) {
+                va = dp[a] * 2;
+                vb = dp[b] * 3;
+                vc = dp[c] * 5;
+                //三者取最小的，即就是当前最小的丑叔，放入dp表中
+                dp[i] = Math.min(Math.min(va, vb), vc);
+                //计算出最新的丑叔后，相应计算丑叔的前者下标需要后移
+                if (va == dp[i]) {
+                    a++;
+                }
+                if (vb == dp[i]) {
+                    b++;
+                }
+                if (vc == dp[i]) {
+                    c++;
+                }
+            }
+            return dp[n - 1];
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(nthUglyNumber(10));
     }
