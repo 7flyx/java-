486136514b776e7d4189f3b29cf92dbd0e5ef791
7flyx <2647282717@qq.com>
2022-03-02 22:03:22 +0800

    剑指offer

---
+++ 20220206/untitled/src/Demo.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Terry
+ * User: Administrator
+ * Date: 2022-03-02
+ * Time: 15:26
+ * Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        int i = 0;
+        Integer j = new Integer(0);
+        System.out.println(i == j);
+        System.out.println(j.equals(i));
+    }
+}
