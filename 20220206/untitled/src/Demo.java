88b20e5fca72605eae07a8b9364258a1c3176c3f
7flyx <2647282717@qq.com>
2022-03-03 20:09:28 +0800

    剑指offer

--- 20220206/untitled/src/Demo.java
+++ 20220206/untitled/src/Demo.java
@@ -7,9 +7,42 @@
  */
 public class Demo {
     public static void main(String[] args) {
-        int i = 0;
-        Integer j = new Integer(0);
-        System.out.println(i == j);
-        System.out.println(j.equals(i));
+        System.out.println(new B().getValue());
+    }
+
+    static class A {
+        protected int value;
+
+        public A(int v) {
+            setValue(v);
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            try {
+                value++;
+                return value;
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            } finally {
+                this.setValue(value);
+                System.out.println(value);
+            }
+            return value;
+        }
+    }
+
+    static class B extends A {
+        public B() {
+            super(5);
+            setValue(getValue() - 3);
+        }
+
+        public void setValue(int value) {
+            super.setValue(2 * value);
+        }
     }
 }
